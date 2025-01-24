d841cd1313c7208c43c16b142ecefe85ddfecdd4
Joseph Gordy <jgordy2424@gmail.com>
2025-01-24 15:53:26 -0500

    Add PlayerChart component for displaying batting order and player performance

---
+++ src/app/components/PlayerChart.jsx
@@ -0,0 +1,42 @@
+import styles from '../page.module.css';
+
+const PlayerChart = ({ playerChart }) => {
+    if (!playerChart) {
+        return null;
+    }
+
+    return (
+        <table className={styles.lineupTable}>
+            <thead>
+                <tr>
+                    <th>Batting Order</th>
+                    <th>Player</th>
+                    {(
+                        <>
+                            {Array.from({ length: 7 }, (_, i) => (
+                                <th key={i}>Inning {i + 1}</th>
+                            ))}
+                        </>
+                    )}
+                </tr>
+            </thead>
+            <tbody>
+                {playerChart.map((player, index) => (
+                    <tr key={player.name + index}>
+                        <td>{index + 1}.</td>
+                        <td>{player.name}</td>
+                        {(
+                            <>
+                                {Array.from({ length: 7 }, (_, i) => (
+                                    <td key={i}>{playerChart[index].positions[i] || "Out"}</td>
+                                ))}
+                            </>
+                        )}
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+};
+
+export default PlayerChart;
