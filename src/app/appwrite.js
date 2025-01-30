a5eb6aab71ff33295066c824e709ab2b7fef67ec
Joseph Gordy <jgordy2424@gmail.com>
2025-01-30 12:04:55 -0500

    Use environment variable for Appwrite project ID in appwrite.js

--- src/app/appwrite.js
+++ src/app/appwrite.js
@@ -4,7 +4,7 @@ export const client = new Client();
 
 client
     .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('679b95f10030c4821c90');
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID);
 
 export const account = new Account(client);
 export { ID } from 'appwrite';
