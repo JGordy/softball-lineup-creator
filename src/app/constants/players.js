6db1be97a79ced6574006fd641d45cb3c981b3dd
Joseph Gordy <jgordy2424@gmail.com>
2025-02-03 15:11:58 -0500

    Add player IDs and initialize positions in player data

--- src/app/constants/players.js
+++ src/app/constants/players.js
@@ -1,84 +1,112 @@
 const players = [{
+    id: '1',
     name: "Stephen",
     preferredPositions: ['Shortstop', 'Pitcher', 'First Base', 'Third Base'],
+    positions: [],
     battingRating: 10,
     fieldRating: 8,
     gender: 'male'
 }, {
+    id: '2',
     name: "Joe",
     preferredPositions: ['Left Field', 'Left Center Field'],
+    positions: [],
     battingRating: 8,
     fieldRating: 10,
     gender: 'male'
 }, {
+    id: '3',
     name: "Whit",
     preferredPositions: ['Left Center Field', 'Left Field', 'Right Center Field'],
+    positions: [],
     battingRating: 8,
     fieldRating: 10,
     gender: 'male'
 }, {
+    id: '4',
     name: "Matt",
     preferredPositions: ['Pitcher', 'Left Center Field', 'Right Center Field'],
+    positions: [],
     battingRating: 7,
     fieldRating: 8,
     gender: 'male'
 }, {
+    id: '5',
     name: "Finn",
     preferredPositions: ['Right Center Field', 'Right Field'],
+    positions: [],
     battingRating: 6,
     fieldRating: 6,
     gender: 'male'
 }, {
+    id: '6',
     name: "Grace",
     preferredPositions: ['Catcher', 'Second Base'],
+    positions: [],
     battingRating: 1,
     fieldRating: 1,
     gender: 'female'
 }, {
+    id: '7',
     name: "MJ",
     preferredPositions: ['Right Field', 'Second Base'],
+    positions: [],
     battingRating: 6,
     fieldRating: 6,
     gender: 'female'
 }, {
+    id: '8',
     name: "Kasey",
     preferredPositions: ['Second Base', 'Shortstop', 'Right Field'],
+    positions: [],
     battingRating: 7,
     fieldRating: 8,
     gender: 'female'
 }, {
+    id: '9',
     name: "Kris",
     preferredPositions: ['First Base', 'Right Field', 'Right Center Field'],
+    positions: [],
     battingRating: 7,
     fieldRating: 9,
     gender: 'male'
 }, {
+    id: '10',
     name: "Nick",
     preferredPositions: ['Third Base', 'Shortstop', 'First Base'],
+    positions: [],
     battingRating: 8,
     fieldRating: 9,
     gender: 'male'
 }, {
+    id: '11',
     name: "Caroline",
     preferredPositions: ['Second Base', 'Right Field'],
+    positions: [],
     battingRating: 5,
     fieldRating: 5,
     gender: 'female'
 }, {
+    id: '12',
     name: "Ethan",
     preferredPositions: ['Left Center Field', 'Right Center Field', 'Right Field'],
+    positions: [],
     battingRating: 5,
     fieldRating: 7,
     gender: 'male'
 }, {
+    id: '13',
     name: "Hayden",
     preferredPositions: ['Left Center Field', 'Right Center Field', 'Right Field'],
+    positions: [],
     battingRating: 6,
     fieldRating: 8,
     gender: 'male'
 }, {
+    id: '14',
     name: "Hannah",
     preferredPositions: ['Catcher', 'Second Base'],
+    positions: [],
     battingRating: 2,
     fieldRating: 2,
     gender: 'female'
