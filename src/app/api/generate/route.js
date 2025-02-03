1598c8f92a4434b6932fa43ffb5b1f30571ed330
Joseph Gordy <jgordy2424@gmail.com>
2025-02-03 15:10:41 -0500

    Refactor API generation logic to include schema and improve response handling

--- src/app/api/generate/route.js
+++ src/app/api/generate/route.js
@@ -2,7 +2,8 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { NextResponse } from "next/server";
 
-import prompt from '../../constants/prompt';
+import prompt from './prompt';
+import schema from './schema.js';
 
 // Create an asynchronous function POST to handle POST 
 // request with parameters request and response.
@@ -12,15 +13,18 @@ export async function POST(req, res) {
         // Access your API key by creating an instance of GoogleGenerativeAI we'll call it GenAI
         const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
-        // Ininitalise a generative model
-        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+        // Initalise a generative model
+        const model = genAI.getGenerativeModel({
+            model: "gemini-1.5-flash",
+            generationConfig: {
+                responseMimeType: "application/json",
+                responseSchema: schema,
+            },
+        });
 
         // Retrieve the data we recieve as part of the request body
         const data = await req.json();
-
-        // Define a prompt varibale
         const body = data.body;
-        console.log({ body });
 
         const updatedPrompt = `
             Generate a softball batting order and fielding chart for the following players, adhering to the specified rules:
@@ -33,16 +37,13 @@ export async function POST(req, res) {
         const result = await model.generateContent(updatedPrompt);
         const response = await result.response;
         const output = await response.text();
+        console.log({ output });
 
-        // Extract the JSON string using a regular expression
-        const jsonMatch = output.match(/```json\n(.*)\n```/s);
-
-        if (jsonMatch) {
-            const jsonString = jsonMatch[1];
-            return NextResponse.json({ output: JSON.parse(jsonString) });
+        if (output) {
+            // Send the llm output as a server reponse object
+            return NextResponse.json({ output: JSON.parse(output) });
         }
 
-        // Send the llm output as a server reponse object
     } catch (error) {
         console.error(error)
     }
